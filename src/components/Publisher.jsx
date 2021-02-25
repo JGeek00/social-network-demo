e7da37e1bacd3e080ced922b4dfe2ecbe7b821f5
Juan Gilsanz Polo <juangilsanz@gmail.com>
2021-02-25 18:31:14 +0100

    Removed unnecesary component imports

--- src/components/Publisher.jsx
+++ src/components/Publisher.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Publisher = ({publishPost, handleWritePost, titleText, bodyText}) => {
     return (
         <div className="publisher">
             <div className="textareaDiv">
-                <input type="text" className="title" name="title" onInput={handleWritePost} placeholder="Post title..." value={titleText}/>
-                <textarea className="textareaPublisher" name="body" placeholder="Write something..." onInput={handleWritePost} value={bodyText}></textarea>
+                <input type="text" className="title" name="title" onInput={handleWritePost} placeholder="Post title..." value={titleText} autoComplete="off"/>
+                <textarea className="textareaPublisher" name="body" placeholder="Write something..." onInput={handleWritePost} value={bodyText} autoComplete="off"></textarea>
                 <div className="publishButtonDiv">
                     <button onClick={publishPost}>
                         <i className="bi bi-chat-left-text"></i>
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const TopBar = ({filteredUsers, searchValue, onSearch, onDeleteSearch}) => {
     return (
