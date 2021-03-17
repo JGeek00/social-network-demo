87bfcc1a9482ddde3697e96b829fd0ca5e136f24
Juan Gilsanz Polo <juangilsanz@gmail.com>
2021-03-17 18:30:57 +0100

    Removed unused imports

--- src/components/PostModal.jsx
+++ src/components/PostModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Modal from 'react-responsive-modal';
 
 const PostModal = ({postModalOpen, closePostModal, postInModal}) => {
